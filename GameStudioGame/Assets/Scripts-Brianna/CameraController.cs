72083d67b1bd06d7371cf657f79454c563f68123
briannaxlee <briannaxlee@gmail.com>
2022-10-02 17:11:35 -0400

    fixed my scene for my branch

--- GameStudioGame/Assets/Scripts-Brianna/CameraController.cs
+++ GameStudioGame/Assets/Scripts-Brianna/CameraController.cs
@@ -6,14 +6,6 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
