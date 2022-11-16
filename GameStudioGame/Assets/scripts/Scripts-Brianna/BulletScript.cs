d5783c80bd526f38aab7f1a780fd06d3fbaef1df
briannaxlee <briannaxlee@gmail.com>
2022-11-16 09:18:53 -0500

    animations for variants are done!

--- GameStudioGame/Assets/scripts/Scripts-Brianna/BulletScript.cs
+++ GameStudioGame/Assets/scripts/Scripts-Brianna/BulletScript.cs
@@ -7,12 +7,15 @@ public class BulletScript : MonoBehaviour
     GameObject target;
     public float speed;
     Rigidbody2D bulletRB;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         bulletRB = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+
         Vector2 moveDirection = (target.transform.position - transform.position).normalized * speed;
         bulletRB.velocity = new Vector2(moveDirection.x, moveDirection.y);
         Destroy(this.gameObject, 2);
