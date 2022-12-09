c961683818e6bbf8aa4aa3036e2eba694c4ff0da
ChristopherRogers1 <60368736+ChristopherRogers1@users.noreply.github.com>
2022-12-09 16:34:09 -0500

    main menu + uploaded assets

---
+++ GameStudioGame/Assets/scripts/Scripts-Chris/OptionsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class OptionsMenu : MonoBehaviour {
+    public Slider musicVolumeSlider;
+    public Slider effectsVolumeSlider;
+ 
+    void Start() {
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        effectsVolumeSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 0.75f);
+    }
+    public void updateMusicVolume() {
+		PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value);
+		AudioListener.volume = PlayerPrefs.GetFloat("MusicVolume");
+	}
+ 
+	public void updateEffectsVolume() {
+		PlayerPrefs.SetFloat("EffectsVolume", effectsVolumeSlider.value);
+		AudioListener.volume = PlayerPrefs.GetFloat("EffectsVolume");
+	}
+}
